77b39f04d82a46d0bee7ee13d42613382ba099c2
tokuyama shouta <toku1031ts@gmail.com>
2020-11-14 15:14:02 +0900

    タスク機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React,{useState} from 'react'
 import "./styles.css";
 
  const App = () => {
+   const [todoText, setTodoText] = useState('');
    const [incompleteTodos, setIncompleteTodos] = useState(['あああああ','良い良いい']);
    const [completeTodos, setCompleteTodos] = useState(['ううううう']);
+
+   const onChangeTodoText = (event) => setTodoText(event.target.value)
+
+   const onClickAdd = () => {
+     if(todoText === "") return;
+      const newTodos = [...incompleteTodos, todoText]
+      setIncompleteTodos(newTodos);
+      setTodoText("");
+   };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="Todoを入力"/>
-        <button>追加</button>
+        <input placeholder="Todoを入力" value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTodo</p>
