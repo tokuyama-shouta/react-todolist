e451581fbf9a31914fb6abfc849a34bed723568e
tokuyama shouta <toku1031ts@gmail.com>
2020-11-14 14:23:50 +0900

    style 適応

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,34 @@ import "./styles.css";
  const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="Todoを入力"/>
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTodo</p>
-        <ul>
-          <div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTodo</p>
+        <ul >
+          <div className="list-row">
             <li>あああああ</li>
             <button>完了</button>
             <button>消去</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>良い良いい</li>
             <button>完了</button>
             <button>消去</button>
           </div>
         </ul>
-        <p>未完了のTodo</p>
-        <ul>
-          <div>
-            <li>ううううう</li>
-            <button>戻す</button>
-          </div>
-        </ul>
+        
+      </div>
+      <div className="complete-area"> 
+        <p className="title">未完了のTodo</p>
+          <ul>
+            <div className="list-row">
+              <li>ううううう</li>
+              <button>戻す</button>
+            </div>
+          </ul>
       </div>
       
     
